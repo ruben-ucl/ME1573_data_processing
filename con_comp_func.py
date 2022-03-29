055e477950947edaab7a0d8ba2a92168c7f4d2b0
ruben-ucl <ruben.lambert-garcia.21@ucl.ac.uk>
2022-03-29 16:41:31 +0100

    Switched con_comp boxes to greyscale

--- con_comp_func.py
+++ con_comp_func.py
@@ -5,6 +5,7 @@ def con_comp(im, connectivity):
     
     # Get connected components with stats
     (numLabels, labels, stats, centroids) = cv2.connectedComponentsWithStats(im, connectivity, cv2.CV_32S)
+    
     con_comp_dict = {'numLabels': numLabels,
                      'labels': labels,
                      'stats': stats,
@@ -13,7 +14,8 @@ def con_comp(im, connectivity):
     
     # Initialise mask to store component locations and RGB image to store bounding boxes
     mask = np.zeros_like(im)
-    output_im = cv2.cvtColor(np.zeros_like(im), cv2.COLOR_GRAY2RGB)
+    output_im = np.zeros_like(im)
+    output_box_mask = np.zeros_like(im)
     
     # Loop through all components
     for i in range(1, numLabels):
@@ -26,17 +28,17 @@ def con_comp(im, connectivity):
         (cX, cY) = centroids[i]
         
         # Set selection criteria for components
-        keepWidth = w > 3 and w < 50
-        keepHeight = h > 3 and h < 50
-        keepArea = area > 30 and area < 1000
+        keepWidth = w > 2 and w < 50
+        keepHeight = h > 2 and h < 50
+        keepArea = area > 5 and area < 1000
         keepY = y > 320 and y < 450
         
         # For components that satisfy conditions: print details, add to mask, and add bounding box to output image
         if all((keepWidth, keepHeight, keepArea, keepY)):
-            print(f'Keeping component {i+1}/{numLabels}\nx: {x}, y: {y}, w: {w}, h: {h}, area: {area}, centroid: {(cX, cY)}')
+            # print(f'Keeping component {i+1}/{numLabels}\nx: {x}, y: {y}, w: {w}, h: {h}, area: {area}, centroid: {(cX, cY)}')
             componentMask = (labels == i).astype("uint8") * 255
             mask += componentMask
-            output_im = cv2.rectangle(output_im, (x, y), (x + w, y + h), (0, 255, 0), 1)
-            bounding_box_mask = output_im != 0
-            
-    return bounding_box_mask, con_comp_dict
+            output_im = cv2.rectangle(output_im, (x, y), (x + w, y + h), 255, 1)
+            output_box_mask = output_im == 255
+    
+    return output_box_mask, con_comp_dict
