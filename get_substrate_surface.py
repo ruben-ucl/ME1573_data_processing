908070cc13661160909acbd400f22bc0874454e6
ruben-ucl <92101404+ruben-ucl@users.noreply.github.com>
2022-10-25 17:27:16 +0100

    Updated to use feret diameter for filtering by edge length

--- get_substrate_surface.py
+++ get_substrate_surface.py
@@ -21,13 +21,14 @@ def median_filter(im, kernel):
     
 def get_edge(im):
     labels = measure.label(im, connectivity=2)
-    props_df = pd.DataFrame(measure.regionprops_table(labels, properties=('label', 'centroid', 'coords')))
-    props_df.sort_values('centroid-0', ascending=False, inplace=True, ignore_index=True)
-    edge_label = props_df.at[0, 'label']
-    edge_coords = props_df.at[0, 'coords'].transpose()
-    edge_mask = labels == edge_label
+    edges = pd.DataFrame(measure.regionprops_table(labels, properties=('label', 'centroid', 'coords', 'feret_diameter_max', 'area')))
+    long_edges = edges[edges['feret_diameter_max'] > 400] # Filter out short edges
+    long_edges_sorted = long_edges.sort_values('centroid-0', ascending=False, inplace=False, ignore_index=True)
+    print(long_edges_sorted.head())
+    edge_label = long_edges_sorted.at[0, 'label']
+    edge_coords = long_edges_sorted.at[0, 'coords'].transpose()
     m, c = np.polyfit(edge_coords[1], edge_coords[0], 1)
-    return edge_mask, m, c, edge_coords # Returns mask of substrate edge pixels, intercept and slope of linear fit, coordinates of actual edge points.
+    return m, c, edge_coords # Returns intercept and slope of linear fit, and coordinates of actual edge points.
     
 def get_surface(im):
     fig, (ax1, ax2) = plt.subplots(2, 1, dpi=300)
@@ -36,12 +37,10 @@ def get_surface(im):
     kernel_height = 5 # default 5
     im_filt = median_filter(im, get_kernel(kernel_width, kernel_height))      # Apply median filter with custom kernel
     
-    im_canny = feature.canny(im_filt, sigma=1.5, high_threshold=80)
-    canny_overlay = 0.7 * im_filt
-    canny_overlay[im_canny] = 255
+    im_canny = feature.canny(im_filt, sigma=1, mode='nearest')
     
-    im_edge, m, c, edge_coords = get_edge(im_canny)
-    ax1.imshow(canny_overlay, cmap='gray')
+    m, c, edge_coords = get_edge(im_canny)
+    ax1.imshow(im_canny, cmap='gray')
     ax1.plot(edge_coords[1], edge_coords[0], 'b', linewidth=0.4)
         
     xx = range(im.shape[1])
@@ -69,24 +68,27 @@ def main():
         trackid = fname[:5] + '0' + fname[-6]
         
         with h5py.File(f, 'a') as file:
-            im = np.array(file['bg_sub_first_30_frames'][-1]) # Take last frame of background subtracted dataset as a 2d array
+            im = np.array(file['ffc_bg_sub_first_40_frames'][-1]) # Take last frame of background subtracted dataset as a 2d array
         
         try:
             m, c, fig = get_surface(im)  # Get slope and left intercept of substrate top surface
+            
+            print(f'Recorded edge equation: y = {m}x + {c}')
+            
+            fig.suptitle(f'{trackid} substrate surface detection')
+            plt.savefig(Path(output_folder, f'{trackid}_substrate_surface.png'))
+            print('Figure saved\n')
+            plt.close('all')
         except KeyError:
             print('ERR: Edge not detected\n')
+            m = None
+            c = None
         
         substrate_surface_dict['trackid'].append(trackid)
         substrate_surface_dict['m'].append(m)
         substrate_surface_dict['c'].append(c)
-        print(f'Recorded edge equation: y = {m}x + {c}')
         
-        fig.suptitle(f'{trackid} substrate surface detection')
-        plt.savefig(Path(output_folder, f'{trackid}_substrate_surface.png'))
-        print('Figure saved\n')
-        plt.close()
-            
-    pd.DataFrame(substrate_surface_dict).to_csv(Path(output_folder, 'substrate_surface_locations.csv'), index=False) # Save numerical results as .csv file
+        pd.DataFrame(substrate_surface_dict).to_csv(Path(output_folder, 'substrate_surface_locations.csv'), index=False) # Save numerical results as .csv file
     
 if __name__ == "__main__":
 	main()
