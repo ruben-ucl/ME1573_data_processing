b05a34dfb75c2f6720664582da742e90a509570b
ruben-ucl <92101404+ruben-ucl@users.noreply.github.com>
2022-11-02 14:49:21 +0000

    Added automatic histogram stretching step before compression to 8-bit

--- bg_sub.py
+++ bg_sub.py
@@ -2,8 +2,10 @@ import h5py, glob
 import numpy as np
 from pathlib import Path
 from skimage import filters
+from skimage import exposure
 from skimage.morphology import disk, ball
 import functools
+from my_funcs import *
 
 __author__ = 'Rubén Lambert-Garcia'
 __version__ = 'v0.2'
@@ -26,57 +28,61 @@ with open('data_path.txt', encoding='utf8') as f:
     print(f'Reading from {filepath}\n')
     
 input_dset_name = 'ff_corrected'
-output_dset_name_prefix = 'ffc'
 
-pre_filt = False
-filt_rad = 3
+logbook_fpath = Path('J:\Logbook_Al_ID19_combined_RLG.xlsx')
 
 # Output information
 mode = 'first_n_frames' # Set to 'first_n_frames' or 'prev_n_frames'
-n = 40
+n = 100
 
 def main(mode, n):
+    logbook = get_logbook(logbook_fpath)
     for f in glob.glob(str(Path(filepath, '*.hdf5'))):
-        print('Reading %s' % Path(f).name)
+        fname = Path(f).name
+        trackid = fname[:5] + '0' + fname[-6]
+        print('Reading %s' % fname)
         try:
             with h5py.File(f, 'a') as file:
                 dset = file[input_dset_name]
                 print('shape: %s, dtype: %s'% (dset.shape, dset.dtype))
                 print('Calculating output')
+                start, _ = get_start_end_frames(trackid, logbook, margin=50)
                 if mode == 'first_n_frames':
                     output_dset = first_n_frames(dset, n)
-                    output_dset_name = f'{output_dset_name_prefix}_bg_sub_first_{n}_frames'
+                    output_dset_name = f'bs-f{n}'
                 if mode == 'prev_n_frames':
-                    output_dset = prev_n_frames(dset, n)
-                    output_dset_name = f'{output_dset_name_prefix}_bg_sub_prev_{n}_frames'
+                    output_dset = prev_n_frames(dset, n, start)
+                    output_dset_name = f'bs-p{n}'
                 file[output_dset_name] = output_dset
                 # transfer_attr(file[input_dset_name], file[output_dset_name], 'element_size_um')
             print('Done\n')
         except OSError as e:
             print('Error: output dataset with the same name already exists - skipping file\n')
 
-def prev_n_frames(dset, n):
-    output_dset = np.zeros_like(dset[n:])
+def prev_n_frames(dset, n, start):
+    output_dset = np.zeros_like(dset)
     dset_mean = np.mean(dset)
+    # dset = np.clip(dset, 1, None)
     for i, frame in enumerate(dset):
-                if i < n:
+                if (i < n) or (i < start):
                     continue
                 else:
                     prev_n_frames_avg = np.clip(np.mean(dset[i-n:i, :, :], axis=0), 1, None)
-                    output_dset[i-n, :, :] = np.clip(frame / prev_n_frames_avg * dset_mean, 0, 255)
+                    bg_sub = frame / prev_n_frames_avg
+                    lp, up = np.percentile(bg_sub, (0.1, 99.9))
+                    bg_sub = exposure.rescale_intensity(bg_sub, in_range=(lp, up))
+                    output_dset[i, :, :] = (bg_sub / np.max(bg_sub) * 255).astype(np.uint8)
     return output_dset
     
 def first_n_frames(dset, n):
-    output_dset = np.zeros_like(dset)
     dset_mean = np.mean(dset)
     first_n_frames_mean = np.clip(np.mean(dset[:n, :, :], axis=0), 1, None).astype(np.uint8) # Set 0 value pixels to 1 to avoid zero division errors
-    if pre_filt == True:
-        first_n_frames_mean = filters.rank.median(first_n_frames_mean, footprint=get_kernel(first_n_frames_mean, filt_rad))
-    for i, frame in enumerate(dset):
-        if pre_filt == True:
-            frame = filters.rank.median(frame, footprint=get_kernel(frame, filt_rad))
-        output_dset[i, :, :] = np.clip(frame/first_n_frames_mean * dset_mean, 0, 255)
-    return output_dset
+    dset = np.clip(dset, 1, None)
+    bg_sub = dset / first_n_frames_mean
+    lp, up = np.percentile(bg_sub, (1, 99))
+    bg_sub = exposure.rescale_intensity(bg_sub, in_range=(lp, up))
+    bg_sub = (bg_sub / np.max(bg_sub) * 255).astype(np.uint8)
+    return bg_sub
     
 def transfer_attr(dset_1, dset_2, attr):    # Copy attribute from dset_1 to dset_2
     data = dset_1.attrs.get(attr)
