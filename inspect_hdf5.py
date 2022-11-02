b3bfd6efdd318f91c7937521a3baece3667a1e77
ruben-ucl <92101404+ruben-ucl@users.noreply.github.com>
2022-11-02 14:59:58 +0000

    Print hdf5 dataset details to terminal with option to delete

--- inspect_hdf5.py
+++ inspect_hdf5.py
@@ -21,8 +21,13 @@ with open('data_path.txt', encoding='utf8') as f:
     filepath = fr'{f.read()}'
     print(f'Reading from {filepath}\n')
     
-repeat_for_all = True
+repeat_for_all = False
 dset_to_delete_all = 'ffcorr'
+
+col_w = [50, 25, 15, 10]
+total_w = np.sum(col_w) + 3
+col_format = '{:<'+str(col_w[0])+'} {:<'+str(col_w[1])+'} {:<'+str(col_w[2])+'} {:<'+str(col_w[3])+'}'
+tab_rule = '-'*total_w
 
 # Iterate through files and datasets to perform filtering and thresholding
 def main():
@@ -32,16 +37,25 @@ def main():
     print('\nFinished - no more files')
         
 def inspect_and_delete(f):
-    print(f, ' Datasets:\n-----------------------------------------------------------------------------------')
+    print(f, f' Datasets:\n{tab_rule}')
     dset_props = {}
-    for dset_name in f.keys():
-        dset = f[dset_name]
-        dset_props[dset_name] = [dset.shape, dset.dtype, dset.nbytes]
-    print('{:<30} {:<25} {:<15} {:<10}'.format('Name', 'Shape', 'Datatype', 'Gigabytes'))
-    print('-----------------------------------------------------------------------------------')
+    for i in f.keys():
+        dset = f[i]
+        try:
+            dset_props[i] = [str(dset.shape), str(dset.dtype), str(round(dset.nbytes/(10**9), 6))]
+        except AttributeError:
+            dset_props[i+'/'] = ['', 'Group', '']
+            for j in f[i].keys():
+                subset = f[i][j]
+                try:
+                    dset_props['    '+j] = [str(subset.shape), str(subset.dtype), str(round(subset.nbytes/(10**9), 6))]
+                except AttributeError:
+                    dset_props[j] = ['', '    Sub-group', '']
+    print(col_format.format('Name', 'Shape', 'Datatype', 'Gigabytes'))
+    print(tab_rule)
     for k, v in dset_props.items():
         shape, dtype, nbytes = v
-        print('{:<30} {:<25} {:<15} {:<10}'.format(k, str(shape), str(dtype), str(round(nbytes/(10**9), 6))))
+        print(col_format.format(k, shape, dtype, nbytes))
     try:
         if repeat_for_all != True:
             dset_to_delete = input('\nEnter name of dataset you would like to delete or \'c\' to continue\n')
