d814229124d004a66d278ba0f9b2e56f6add7a97
ruben-ucl <92101404+ruben-ucl@users.noreply.github.com>
2022-11-02 14:59:26 +0000

    Threshold image stack based on values in masked area

--- thresh.py
+++ thresh.py
@@ -3,12 +3,13 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 from skimage import filters
+from my_funcs import *
 
 print = functools.partial(print, flush=True) # Re-implement print to fix issue where print statements do not show in console until after script execution completes
 
-input_dset_name = 'ffc_bg_sub_prev_5_frames_hist_eq_med_filt_r3'
+input_dset_name = 'ffc_bg_sub_prev_10_frames_hist_eq_med_filt_r3'
 
-output_dset_name = 'keyhole_binary_yen'
+output_dset_name = 'bgs_prev_10/keyhole_binary_tri'
 
 # Read data folder path from .txt file
 with open('data_path.txt', encoding='utf8') as f:
@@ -20,36 +21,33 @@ substrate_surface_measurements_fpath = Path(filepath, 'substrate_surface_measure
 def threshold(dset, trackid):
     a = np.array(dset)
     a_masked, vals = mask_substrate(dset, trackid)
-    # a_masked[a_masked == 0] = 255
-    # thresh = filters.threshold_minimum(a)     # Does not work the same as ImageJ, different implementation of histogram smoothing
     print('Calculating threshold')
-    thresh_offset = -5
-    thresh = filters.threshold_yen(vals) + thresh_offset
-    # thresh = filters.threshold_otsu(a)
-    print(f'Threshold calulated by Yen method with offset of {thresh_offset}: {thresh}')
+    thresh_offset = 0   # Defaults for moving window bg sub: 5 frames: Yen -5, 10 frames: Yen -25?
+    thresh = triangle(vals, thresh_offset)
+    # thresh = yen(vals, thresh_offset)
     output_dset = np.zeros_like(dset)
     mask = a_masked > thresh
     output_dset[mask] = 255
     return output_dset
 
+def triangle(vals, offset):
+    thresh = filters.threshold_triangle(vals) + offset
+    print(f'Threshold calulated by Triangle method with offset of {offset}: {thresh}')
+    return thresh
+    
+def yen(vals, offset):
+    thresh = filters.threshold_yen(vals) + offset
+    print(f'Threshold calulated by Yen method with offset of {offset}: {thresh}')
+
 def mask_substrate(dset, trackid):  # Function to mask the images so that only the substrate region is used for thresholding. Sets region above to zero.
     print('Masking ROI')
-    substrate_mask = np.ones_like(dset[0], dtype=bool)
-    substrate_surface_df = pd.read_csv(substrate_surface_measurements_fpath, index_col='trackid')
-    m = substrate_surface_df.at[trackid, 'm']
-    c = substrate_surface_df.at[trackid, 'c']
-    print(f'Substrate edge equation retrieved: y = {m}x + {c}')
-    n_rows, n_cols = substrate_mask.shape
-    print('Calculating mask dimensions')
-    for x in range(n_cols):
-        surface_height = round(m * x + c)
-        substrate_mask[surface_height:, x] = False
+    substrate_mask = get_substrate_mask(dset[0].shape, substrate_surface_measurements_fpath, trackid)
     
     print('Creating 3d mask from 2d slice')
     substrate_mask_3d = np.stack([substrate_mask for i in range(len(dset))])
     print('Setting pixel values above substrate surface to zero')
     stack_masked = np.array(dset)
-    stack_masked[substrate_mask_3d] = 0
+    stack_masked[np.invert(substrate_mask_3d)] = 0
     print('Extracting substrate grey values for threshold calculation')
     val_inds = np.nonzero(stack_masked)
     vals = stack_masked[val_inds]
