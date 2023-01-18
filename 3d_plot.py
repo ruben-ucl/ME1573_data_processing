91ca0d89bd366bc7c03c00ac16d316b826b0ad4d
ruben-ucl <ruben.lambert-garcia.21@ucl.ac.uk>
2023-01-18 15:28:29 +0000

    Updated markers and cmap assignment

--- 3d_plot.py
+++ 3d_plot.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-log_path = Path(r"C:\Users\rlamb\Documents\UCL\Experiments\ESRF LTP 2 logbook AlSi10Mg only.xlsx")
+log_path = Path(r'C:\Users\rlamb\Documents\UCL\Experiments\ESRF LTP 2 logbook AlSi10Mg only.xlsx')
 
 log = pd.read_excel(log_path)
 
@@ -13,40 +13,52 @@ ax = fig.add_subplot()
 # ax = fig.add_subplot(projection=projection)
 
 # filter only PWM welding cases
-welding = log["Powder material"] == "none"
+welding = log['Powder material'] == 'None'
 
 # filter only CW powder cases
-cw = log["point jump delay [μs]"] == 0
+cw = log['Point jump delay [μs]'] == 0
 
-# filter only Layer 5 cases
-L5 = log["Layer"] == 5
+# filter only Layer 1 cases
+L1 = log['Layer'] == 1
+
+# filter only Al10SiMg cases
+Al10SiMg = log['Substrate material'] == 'Al10SiMg'
 
 # log_red = log[welding]
-log_red = log[cw]
-# log_red = log[L5]
+log_red = log[cw & Al10SiMg]
+# log_red = log[L1]
 
-for power, m in [(300, "v"), (325, "*"), (350, "."), (375, "+"), (400, "D"), (450, "s"), (475, "x"), (500, "^")]:
-    log_red_pwr = log_red[log_red["Power [W]"] == power]
+regime_markers = [('unstable keyhole', 'o'),
+                  ('keyhole flickering', 's'),
+                  ('quasi-stable keyhole', '^'),
+                  ('quasi-stable vapour depression', 'D'),
+                  ('conduction', 'v')
+                  ]
+
+for regime, m in regime_markers:
+    reg_bool = log_red['Melting regime'] == regime
+    log_red_reg = log_red[reg_bool]
+    reg_pwr = log_red_reg['Power [W]']
     
     if projection == '3d':
-        xs = log_red_pwr["Exposure time [μs]"]
-        ys = log_red_pwr["Point distance [μm]"]
-        zs = log_red_pwr["Power [W]"]
+        xs = log_red_reg['Exposure time [μs]']
+        ys = log_red_reg['Point distance [μm]']
+        zs = log_red_reg['Power [W]']
     
-        ax.scatter(xs, ys, zs, marker = m)
+        ax.scatter(xs, ys, zs, marker=m, c=reg_pwr, cmap='jet')
     
         ax.set_xlabel('Exposure time [μs]')
         ax.set_ylabel('Point distance [μm]')
-        ax.set_zlabel('Power [W]')
+        ax.set_zlabel('Avg. power [W]')
         
     else:
-        xs = log_red_pwr["scan speed [mm/s]"]
-        ys = log_red_pwr["Power [W]"]
+        xs = log_red_reg['Scan speed [mm/s]']
+        ys = log_red_reg['Power [W]']
         
-        ax.scatter(xs, ys, marker = m)
+        ax.scatter(xs, ys, marker = m, c=reg_pwr, cmap='jet')
         
-        ax.set_xlabel("Scan speed [mm/s]")
-        ax.set_ylabel("Power [W]")
+        ax.set_xlabel('Scan speed [mm/s]')
+        ax.set_ylabel('Power [W]')
 
 ax.zticks = [300, 350, 400, 450, 500]
 ax.yticks = [20, 40, 60, 80, 100]
