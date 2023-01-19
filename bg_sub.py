13f76631f82a14470c8ba91b71f196edf98be3a1
ruben-ucl <92101404+ruben-ucl@users.noreply.github.com>
2023-01-19 11:52:29 +0000

    New 8-bit conversion method and new functionality for moving window bg_sub

--- bg_sub.py
+++ bg_sub.py
@@ -8,13 +8,14 @@ import functools
 from my_funcs import *
 
 __author__ = 'Rubén Lambert-Garcia'
-__version__ = 'v0.2'
+__version__ = 'v0.3'
 
 '''
 CHANGELOG
     v0.1 - Carries out background subtraction on specified hdf5 file datasets in either 'previous frame' or 'first n frames' mode
            and apppends output to the original file as a new dataset
     v0.2 - Added moving window background subtraction mode 'prev_n_frames'
+    v0.3 - Rewritten algorithms 
            
 INTENDED CHANGES
     - Add more background subtraction options
@@ -32,8 +33,9 @@ input_dset_name = 'ff_corrected'
 logbook_fpath = Path('J:\Logbook_Al_ID19_combined_RLG.xlsx')
 
 # Output information
-mode = 'first_n_frames' # Set to 'first_n_frames' or 'prev_n_frames'
-n = 100
+mode = 'prev_n_frames_skip_m' # Set to 'first_n_frames', 'prev_n_frames' or 'prev_n_frames_skip_m'
+n = 5 
+m = 5
 
 def main(mode, n):
     logbook = get_logbook(logbook_fpath)
@@ -46,52 +48,66 @@ def main(mode, n):
                 dset = file[input_dset_name]
                 print('shape: %s, dtype: %s'% (dset.shape, dset.dtype))
                 print('Calculating output')
-                start, _ = get_start_end_frames(trackid, logbook, margin=50)
                 if mode == 'first_n_frames':
                     output_dset = first_n_frames(dset, n)
                     output_dset_name = f'bs-f{n}'
-                if mode == 'prev_n_frames':
-                    output_dset = prev_n_frames(dset, n, start)
+                elif mode == 'prev_n_frames':
+                    output_dset = prev_n_frames(dset, n)
                     output_dset_name = f'bs-p{n}'
+                elif mode == 'prev_n_frames_skip_m':
+                    output_dset = prev_n_frames(dset, n, m)
+                    output_dset_name = f'bs-p{n}-s{m}'
                 file[output_dset_name] = output_dset
                 # transfer_attr(file[input_dset_name], file[output_dset_name], 'element_size_um')
             print('Done\n')
         except OSError as e:
             print('Error: output dataset with the same name already exists - skipping file\n')
 
-def prev_n_frames(dset, n, start):
-    output_dset = np.zeros_like(dset)
-    dset_mean = np.mean(dset)
-    # dset = np.clip(dset, 1, None)
-    for i, frame in enumerate(dset):
-                if (i < n) or (i < start):
-                    continue
-                else:
-                    prev_n_frames_avg = np.clip(np.mean(dset[i-n:i, :, :], axis=0), 1, None)
-                    bg_sub = frame / prev_n_frames_avg
-                    lp, up = np.percentile(bg_sub, (0.1, 99.9))
-                    bg_sub = exposure.rescale_intensity(bg_sub, in_range=(lp, up))
-                    output_dset[i, :, :] = (bg_sub / np.max(bg_sub) * 255).astype(np.uint8)
-    return output_dset
-    
 def first_n_frames(dset, n):
-    dset_mean = np.mean(dset)
-    first_n_frames_mean = np.clip(np.mean(dset[:n, :, :], axis=0), 1, None).astype(np.uint8) # Set 0 value pixels to 1 to avoid zero division errors
-    dset = np.clip(dset, 1, None)
-    bg_sub = dset / first_n_frames_mean
-    lp, up = np.percentile(bg_sub, (1, 99))
-    bg_sub = exposure.rescale_intensity(bg_sub, in_range=(lp, up))
-    bg_sub = (bg_sub / np.max(bg_sub) * 255).astype(np.uint8)
-    return bg_sub
+    first_n_frames_avg = np.clip(np.median(dset[:n], axis=0), 1, None) # Set 0 value pixels to 1 to avoid zero division errors
+    bg_sub = dset / first_n_frames_avg
+    # view_histogram(bg_sub, show_std=True, title='bg_sub')
+    bg_sub_filt = median_filt(bg_sub, kernel=disk(radius=3))
+    # view_histogram(bg_sub_filt, show_std=True, title='bg_sub_filt')
+    output_dset_8bit = rescale_to_8bit(bg_sub_filt)
+    # view_histogram(output_dset_8bit, show_std=True, title='output_dset_8bit')
+    return output_dset_8bit
+
+def prev_n_frames(dset, n, m=0):
+    output_dset = np.zeros(dset.shape, dtype=np.float64)
+    for i, frame in enumerate(dset):
+                print(f'Working on frame {i+1}', end='\r')
+                if (i < n+m):
+                    prev_n_frames_avg = np.clip(np.median(dset[:n], axis=0), 1, None)
+                else:
+                    prev_n_frames_avg = np.clip(np.median(dset[i-m-n:i-m], axis=0), 1, None)
+                bg_sub = frame / prev_n_frames_avg
+                output_dset[i] = bg_sub
+    bg_sub_filt = median_filt(output_dset, kernel=disk(radius=3))
+    output_dset_8bit = rescale_to_8bit(bg_sub_filt)
+    # view_histogram(output_dset_8bit[-100])
+    
+    return output_dset_8bit
+
+def rescale_to_8bit(dset):
+    # clip values outside +- 3 sigma (99.7 % confidence interval) and rescale image to 8bit
+    # For lower limit values <0, clip to 0
+    print('Stretching stack histogram and rescaling values to 8-bit')
+    bg_sub_mean = np.mean(dset)
+    bg_sub_std = np.std(dset)
+    a = np.clip(bg_sub_mean - 3 * bg_sub_std, 0, None)
+    b = bg_sub_mean + 3 * bg_sub_std                
+    output_dset_norm = np.clip((dset - a) / (b - a), 0, 1)
+    # view_histogram(output_dset_norm, show_std=True, title='Normalised')
+    
+    output_dset_8bit = (output_dset_norm * 255).astype(np.uint8)
+    # view_histogram(output_dset_8bit, show_std=True, title='8bit')
+    
+    return output_dset_8bit
     
 def transfer_attr(dset_1, dset_2, attr):    # Copy attribute from dset_1 to dset_2
     data = dset_1.attrs.get(attr)
     dset_2.attrs.create(attr, data)
-
-def get_kernel(data, radius):
-    footprint_function = disk if data.ndim == 2 else ball
-    filter_kernel = footprint_function(radius=radius)
-    return filter_kernel
 
 main(mode, n)
     
