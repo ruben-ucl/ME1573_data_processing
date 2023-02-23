dd1d1f305fde3ebc310307cfbba8b9ec87a7290d
ruben-ucl <92101404+ruben-ucl@users.noreply.github.com>
2023-02-23 10:44:51 +0000

    Added sum function with false colour plot

--- z_project.py
+++ z_project.py
@@ -1,10 +1,12 @@
-import h5py, glob, os, imageio
+import h5py, glob, os, imageio, functools
 import numpy as np
 from pathlib import Path
 from skimage import filters
 from skimage.morphology import disk
 from skimage import exposure
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib_scalebar.scalebar import ScaleBar
 
 __author__ = 'Rubén Lambert-Garcia'
 __version__ = 'v0.1'
@@ -15,20 +17,24 @@ CHANGELOG
            
 INTENDED CHANGES
     - 
-    
+
 '''
+print = functools.partial(print, flush=True) # Re-implement print to fix issue where print statements do not show in console until after script execution completes
+
 # Input informaton
 # Read data folder path from .txt file
 with open('data_path.txt', encoding='utf8') as f:
     filepath = fr'{f.read()}'
     print(f'Reading from {filepath}\n')
     
-input_dset_name = 'bs-p5-s5_lagrangian_meltpool'
+input_dset_name = 'bs-f40_lagrangian_meltpool'
 
-frame_reduction_factor = 1                               # Set to 1 to use all frames
-filter_radius = None
-mode = 'save'                                            # Set to 'view' or 'save'
-proj_mode = 'median'                                     # 'median' or 'max'
+frame_reduction_factor = 1                              # Set to 1 to use all frames
+filter_radius = None                                    # Median filter radius, set to None for no filter
+mode = 'save'                                        # Set to 'view' or 'save'
+proj_mode = 'mean'                                       # 'median', 'mean' or 'max'
+skip_frames_end = 45                                    # For Lagrangian videos use 45, set to 0 to use all frames
+skip_start_frames = 40
 
 reduction_txt = f'_x{frame_reduction_factor}_stack_reduction' if frame_reduction_factor != 1 else ''
 folder_name = f'{input_dset_name}_z_projection_{proj_mode}{reduction_txt}'
@@ -37,7 +43,7 @@ folder_path = Path(filepath, 'z-project_images', folder_name)
 def main():
     for file in glob.glob(str(Path(filepath, '*.hdf5'))):
         with h5py.File(file, 'a') as f:
-            dset = f[input_dset_name]
+            dset = f[input_dset_name][skip_start_frames:-skip_frames_end]
             trackid = Path(file).name[:-5]
             if mode == 'save':
                 output_filename = f'{trackid}_{input_dset_name}_z_projection_{proj_mode}.png'
@@ -64,20 +70,35 @@ def main():
             print('Creating z-projection')
             if proj_mode == 'max':
                 output_im = np.amax(dset_filt, axis=0)
-                output_im = exposure.equalize_hist(output_im)
-            if proj_mode == 'median':
+                # output_im = exposure.equalize_hist(output_im)
+            elif proj_mode == 'median':
                 output_im = np.median(dset_filt, axis=0)
+            elif proj_mode == 'mean':
+                output_im = np.mean(dset_filt, axis=0)
             
+            # Plot figure
+            plt.rcParams.update({'font.size': 8})
+            fig, ax = plt.subplots(figsize=(4, 2), dpi=600, tight_layout=True)
+            im = ax.imshow(output_im, cmap='viridis', vmin=110, vmax=140)
+            scalebar = ScaleBar(dx=4.3, units='um', location='lower left', width_fraction=0.02)
+            plt.gca().add_artist(scalebar)
+            divider = make_axes_locatable(ax)
+            cax = divider.append_axes("right", size="3%", pad=0.05)
+            plt.colorbar(im, cax=cax)
+            cax.set_ylabel('Mean intensity')
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
+            fig.suptitle(trackid)
+            
+            # Save or display figure
             if mode == 'save':
                 if not os.path.exists(folder_path):
                     os.makedirs(folder_path)
-                
-                imageio.imwrite(output_filepath, output_im)
+                plt.savefig(output_filepath)
                 print(f'Image saved to {output_filepath}')
             else:
-                plt.imshow(output_im, cmap='gray')
                 plt.show()
-                
+            
         print('Done\n')
 
 if __name__ == "__main__":
