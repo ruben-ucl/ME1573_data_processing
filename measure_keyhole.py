Fixed output units and added functionality for cropped and full-frame datasets

measure_keyhole.py
@@ -3,29 +3,31 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from skimage import measure
-from skimage import morphology
+from skimage import measure, morphology
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from my_funcs import *
 
 __author__ = 'Rubén Lambert-Garcia'
-__version__ = 'v0.1'
+__version__ = 'v1.0'
 
-"""
+'''
 CHANGELOG
-    v1.0 - Measures keyhole binary images to extract geometry metrics
+    v0.1 - Measures keyhole binary images to extract geometry metrics
          - Outputs individual .csv files with full measurements for each track
          - Outputs summary .csv file containing summary stats for all tracks
+    v1.0 - Switched output units to um instead of px
+         - Works on full frame and Lagrangian cropped datasets
     
 INTENDED CHANGES
     - 
-"""
+'''
 
 """Controls"""
-input_dset_name = 'bs-p5-s5_tri+35_lagrangian_keyhole_refined'
+input_dset_name = 'keyhole_bin'
 save_mode = 'save' # Set to 'preview' or 'save'
 frame_mode = 'cropped' # Set to 'full_frame' or 'cropped'
 plot_keyholes = False
+ignore_last_n_frames = 40
 
 um_per_pix = 4.3
 capture_framerate = 40000 # fps
@@ -41,6 +43,7 @@ def main():
     # Initialise dictionary for storing summary statistics
     keyhole_data_summary = {'trackid': [],
                             'n_frames': [],
+                            'n_samples': [],
                             'area_mean': [],
                             'area_sd': [],
                             'max_depth_mean': [],
@@ -50,15 +53,18 @@ def main():
                             'depth_at_max_length_mean': [],
                             'depth_at_max_length_sd': [],
                             'apperture_length_mean': [],
-                            'apperture_length_sd': [],
+                            'apperture_length_sd': []
                             }
     # Iterate through files measuring keyholes frame by frame
-    for file in glob.glob(str(Path(filepath, '*.hdf5'))):
+    for file in sorted(glob.glob(str(Path(filepath, '*.hdf5')))):
         fname = Path(file).name
         print('\nReading file %s' % fname)
         trackid = fname[:5] + '0' + fname[-6]
         with h5py.File(file, 'a') as f:
-            dset = f[input_dset_name]
+            try:
+                dset = f[input_dset_name]
+            except KeyError:
+                continue
             n_frames = len(dset)
             keyhole_data = {'time': [],
                             'centroid': [],
@@ -66,14 +72,14 @@ def main():
                             'max_depth': [],
                             'max_length': [],
                             'depth_at_max_length': [],
-                            'apperture_length': []
+                            'apperture_length': [],
                             }
             # f1, f2 = get_start_end_frames(trackid, logbook, margin=3, start_frame_offset=5)
             f1 = 0
             # frame_inds = range(f1, f2)
             # len_frame_inds = len(frame_inds)
-            len_frame_inds = len(dset)
-            for i, im in enumerate(dset):
+            len_frame_inds = len(dset[:-ignore_last_n_frames])
+            for i, im in enumerate(dset[:-ignore_last_n_frames]):
                 # Get timestamp of frame
                 time = i * 1/capture_framerate * 1000 # ms
                 keyhole_data['time'].append(time)
@@ -109,7 +115,7 @@ def main():
         else:
             print(keyhole_data[100:110])
     if save_mode == 'save':
-        pd.DataFrame(keyhole_data_summary).to_csv(Path(output_folder, 'keyhole_measurements_summary_v2.csv'))
+        pd.DataFrame(keyhole_data_summary).to_csv(Path(output_folder, 'keyhole_measurements_summary.csv'))
 
 def generate_summary_stats(keyhole_data, keyhole_data_summary, trackid):
     print('Calculating summary stats')
@@ -121,6 +127,8 @@ def generate_summary_stats(keyhole_data, keyhole_data_summary, trackid):
             data_nonzero = data
         else:
             data_nonzero = data[np.nonzero(data)]
+        if col_name == 'area':
+            keyhole_data_summary['n_samples'].append(len(data_nonzero))
         mean = data_nonzero.mean()
         keyhole_data_summary[f'{col_name}_mean'].append(mean)
         sd = data_nonzero.std()
@@ -178,17 +186,20 @@ def get_keyhole_measurements(im, trackid, frame_n):
                 depths_px.append(0)
     
     # Convert from pixels to micrometers
-    # output_um = (x * um_per_pix for x in [max_depth_px, max_length_px, depth_at_max_length_px, apperture_length_px])
-    output_px = (max_depth_px, max_length_px, depth_at_max_length_px, apperture_length_px)
+    output_px = [max_depth_px, max_length_px, depth_at_max_length_px, apperture_length_px]
+    output_um = [x * um_per_pix for x in output_px]
     
     if plot_keyholes == True:
-        print(f'max_depth = {max_depth_px}\nmax_length = {max_length_px}\ndepth_at_max_length = {depth_at_max_length_px}\napperture_length = {apperture_length_px}')
+        print(f'Plotting frame {frame_n}')
+        print(f'   max_depth = {round(output_um[0], 1)} um\n', 
+              f'   max_length = {round(output_um[1], 1)} um\n',
+              f'   depth_at_max_length = {round(output_um[2], 1)} um\n',
+              f'   apperture_length = {round(output_um[3], 1)} um\n'
+              )
         keyhole_dimensions_plot(im, trackid, frame_n, lengths_px, depths_px, min_col, max_col, min_row, max_row)
-    
-    return output_px
+    return output_um
     
 def keyhole_dimensions_plot(im, trackid, frame_n, lengths, depths, min_col, max_col, min_row, max_row):
-    print('Plotting')
     # Get image of keyhole cropped to its bounding box
     keyhole_cropped = im[min_row:max_row, min_col:max_col]
     (y_lim, x_lim) = keyhole_cropped.shape