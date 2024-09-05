2bf63add1a718f2e6b4b55963406fd3afdb4343e
ruben-ucl <92101404+ruben-ucl@users.noreply.github.com>
2024-09-05 13:13:03 +0100

    Create timeseries_compare.py
    plot two timeseries .csv datasets from different directories with filtering options

---
+++ vis/timeseries_compare.py
@@ -0,0 +1,88 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import glob, functools
+from pathlib import Path
+from scipy.signal import medfilt, find_peaks
+
+print = functools.partial(print, flush=True) # Re-implement print to fix issue where print statements do not show in console until after script execution completes
+
+__author__ ='Rubén Lambert-Garcia'
+__version__ = '1.0'
+
+##################################################################################################
+
+path1 = r'J:\ESRF ME1573 LTP 6 Al data HDF5\ffc\keyhole_measurements_lagrangian'
+label1 = 'max_depth'
+
+path2 = r'J:\ESRF ME1573 LTP 6 Al data HDF5\ffc\fkw_angle_measurements'
+label2 = 'fkw_angle'
+
+med_filt_window = None # None or odd-valued int
+running_mean_window = 9 # None or odd-valued int
+
+labels = {'area': ['Keyhole area', ' [μm\u00b2]'],
+          'max_depth': ['Keyhole depth', ' [μm]'],
+          'max_length': ['Keyhole length', '\n[μm]'],
+          'AR': ['Keyhole aspect ratio', ',\nd/l'],
+          'Ra': ['Roughness, Ra', ' [μm]'],
+          'fkw_angle': ['Keyhole front wall angle', ' [°]']
+          }
+
+##################################################################################################
+
+def main():
+    files = sorted(glob.glob(str(Path(path1, '1*v2.csv'))))
+    all_peaks = []
+    for f1 in files:
+        f1name = Path(f1).name
+        print('\nReading %s' % f1name)
+        trackid = f1name[:7]
+        f2 = glob.glob(str(Path(path2, f'{trackid}*raw.csv')))[0]
+        
+        df1 = pd.read_csv(f1, index_col=0)
+        df2 = pd.read_csv(f2, index_col=0)
+        t1 = df1['time'][:-running_mean_window+1]
+        t2 = df2['time'][:-running_mean_window+1]
+        sr1 = 504 # kHz
+        sr2 = sr1
+        var1 = get_data(label1, df1)
+        var2 = get_data(label2, df2)
+        
+        if med_filt_window != None:
+            var1 = medfilt(var1, med_filt_window)
+            var2 = medfilt(var2, med_filt_window)
+        if running_mean_window != None:
+            var1 = np.convolve(var1, np.ones(running_mean_window)/running_mean_window, mode='valid')
+            var2 = np.convolve(var2, np.ones(running_mean_window)/running_mean_window, mode='valid')
+        
+        plt.rcParams.update({'font.size': 8})
+        fig = plt.figure(figsize=(6.3, 3.15), dpi=300, tight_layout=True)
+        fig.suptitle(f'{trackid}: {labels[label1][0]}, {labels[label2][0]}')
+        
+        ax1 = fig.add_subplot(111)
+        # ax1.plot(t1, var1, '-', c='tab:blue', lw=0.8, marker='o', markersize=2, label=labels[label1][0])
+        ax1.plot(t1, var1, '-', c='tab:blue', lw=0.8, markersize=2, label=labels[label1][0])
+        ax1.set_xlabel('Time [ms]')
+        # ax1.set_xlim(0.08, 0.2)
+        ax1.set_ylabel(labels[label1][0]+labels[label1][1], c='tab:blue')
+        ax1.legend(loc='lower left', bbox_to_anchor=(0.01, 0.1), framealpha=0)
+        
+        ax2 = ax1.twinx()
+        # ax2.plot(t2, var2, '--', c= 'tab:red', lw=0.8, marker='x', markersize=3, label=labels[label2][0])
+        ax2.plot(t2, var2, '--', c= 'tab:red', lw=0.8, markersize=3, label=labels[label2][0])
+        ax2.set_ylabel(labels[label2][0]+labels[label2][1], c='tab:red')
+        ax2.legend(loc='lower left', bbox_to_anchor=(0.01, 0.02), framealpha=0)
+        
+        plt.show()
+        # plt.savefig(str(Path(path1, f'{trackid}_KH_{label1}_and_{label2}')))
+        plt.close()
+
+def get_data(label, df):
+    var = np.nan_to_num(np.divide(df['max_depth'], df['max_length'])) if label == 'AR' else df[label]
+    var *= 4.3**2 if label == 'area' else 1 # convert area from px to um^2
+    return var
+
+if __name__ == '__main__':
+    main()
