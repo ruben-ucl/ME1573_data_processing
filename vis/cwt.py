6bedefa2dd378e78b30e785a1e9cbac6f3ad7c16
ruben-ucl <ruben.lambert-garcia.21@ucl.ac.uk>
2025-02-26 15:46:58 +0000

    Added a signal smoothing option

--- vis/cwt.py
+++ vis/cwt.py
@@ -12,22 +12,34 @@ from tools import get_paths, printProgressBar
 folder = get_paths()['hdf5']
 
 group, time, series, colour = ('AMPM', 'Time', 'Photodiode1Bits', 'r')
+running_mean_window = 4
 
-mode = 'preview'
+mode = 'preview' # 'preview' or 'save'
 
-files = sorted(glob.glob(f'{folder}/*.hdf5'))
+def plot_wavelet():    
+    [psi, x] = pywt.ContinuousWavelet(wavelet).wavefun(level=10)
+    plt.plot(x, psi)
+    plt.show()
+
+files = sorted(glob.glob(f'{folder}/0110_01.hdf5'))
 for i, filepath in enumerate(files):
     trackid = Path(filepath).name[:7]
     
     with h5py.File(filepath, 'r') as file:
         t = np.array(file[f'{group}/{time}'])[450:-450]
         s = np.array(file[f'{group}/{series}'])[450:-450]
+        if running_mean_window != None:
+            s = np.convolve(s, np.ones(running_mean_window)/running_mean_window, mode='valid')
+            t = t[:-running_mean_window+1]
         xray_im = np.array(file['bs-f40'])[-1]
         
-        scales = np.logspace(1, 7, num=256, base=2, endpoint=True)
+        scales = np.logspace(2, 7, num=256, base=2, endpoint=True)
         wavelet = "cmor1.5-1.0"
         sampling_period = round(t[1]-t[0], 9)
         sampling_rate = round(1/sampling_period, 7)
+        f = pywt.scale2frequency(wavelet, 1)/sampling_period
+        
+        plot_wavelet()
         
         cwtmatr, freqs = pywt.cwt(s, scales, wavelet, sampling_period=sampling_period)
         cwtmatr = np.abs(cwtmatr[:-1, :-1])
@@ -46,14 +58,14 @@ for i, filepath in enumerate(files):
         ax1.set_xlabel('Time [ms]')
         
         t_ax, f_ax = np.meshgrid(t*1000, freqs/1000)
-        pcm = ax2.pcolormesh(t_ax, f_ax, cwtmatr)
+        pcm = ax2.pcolormesh(t_ax, f_ax, cwtmatr, cmap='jet')
         ax2.set_yscale('log', base=2)
         ax2.yaxis.set_major_formatter(mticker.FormatStrFormatter('%.0f'))
         ax2.set_xlabel('Time [ms]')
         ax2.set_ylabel('Frequency [kHz]')
         fig.colorbar(pcm, cax=ax2b, label='Intensity')
         
-        ax3.imshow(xray_im[150:450], cmap='gray')
+        ax3.imshow(xray_im[150:450], cmap='gray')  # xray_im[150:450] for full frame 40 kHz
         scalebar = ScaleBar(4.3,
             "µm",
             length_fraction = 0.15,
@@ -68,17 +80,14 @@ for i, filepath in enumerate(files):
         plt.tight_layout()
         
         if mode == 'save':
-            if not os.path.exists(f'{folder}/CWT'):
-                os.makedirs(f'{folder}/CWT')
-            plt.savefig(f'{folder}/CWT/{trackid}_{series}_CWT.png')
+            if not os.path.exists(f'{folder}/CWT_jet'):
+                os.makedirs(f'{folder}/CWT_jet')
+            plt.savefig(f'{folder}/CWT_jet/{trackid}_{series}_CWT.png')
         else:
             plt.show()
         plt.close()
         
-        printProgressBar(i, len(files), prefix='Progress', suffix=trackid)
+        printProgressBar(i+1, len(files), prefix='Progress', suffix=trackid)
 
-def plot_wavelet():    
-    [psi, x] = pywt.ContinuousWavelet(wavelet).wavefun(level=10)
-    plt.plot(x, psi)
-    plt.show()
+
     
